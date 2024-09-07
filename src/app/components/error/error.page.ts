e261152e7b74bff051765af826ee53c4971a9abd
RoyDevTEAM <roidercito12345@gmail.com>
2024-09-06 20:48:18 -0400

    falta archivo

--- src/app/components/error/error.page.ts
+++ src/app/components/error/error.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Network } from '@capacitor/network';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-error',
@@ -6,10 +8,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./error.page.scss'],
 })
 export class ErrorPage implements OnInit {
+  isConnected: boolean = true;
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+    this.checkConnection();
   }
 
+  async checkConnection() {
+    const status = await Network.getStatus();
+    this.isConnected = status.connected;
+    
+    if (this.isConnected) {
+      // Redirigir a HomePage si la conexión es restablecida
+      this.router.navigate(['/home']);
+    } else {
+      // Realizar acciones si no hay conexión
+      console.log('No hay conexión a Internet.');
+    }
+  }
+
+  retryConnection() {
+    this.checkConnection();
+  }
 }
