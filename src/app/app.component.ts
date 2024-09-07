32e17f03c5500e969365c1974b809ce9cfa1a62c
RoyDevTEAM <roidercito12345@gmail.com>
2024-09-07 17:49:32 -0400

    animacion una ves6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router'; // Importar Router
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   showSplashScreen = true; // Inicialmente mostrar el splash screen
 
-  constructor() {
+  constructor(private router: Router) {} // Inyectar Router en el constructor
+
+  ngOnInit() {
     this.initializeApp();
   }
 
   initializeApp() {
-    setTimeout(() => {
-      // Aplicar clase de transición para ocultar el splash screen
-      const splashElement = document.querySelector('app-splash-screen');
-      if (splashElement) {
-        splashElement.classList.add('hidden');
-      }
-  
-      // Después de la transición, quitar el splash screen
+    // Verificar si ya se ha mostrado el splash screen
+    const hasSeenSplashScreen = localStorage.getItem('hasSeenSplashScreen');
+    
+    if (!hasSeenSplashScreen) {
       setTimeout(() => {
-        this.showSplashScreen = false;
-      }, 500); // Esperar a que la transición termine (0.5s)
-    }, 3000); // Mostrar el splash screen por 3 segundos
+        // Aplicar clase de transición para ocultar el splash screen
+        const splashElement = document.querySelector('app-splash-screen');
+        if (splashElement) {
+          splashElement.classList.add('hidden'); // Asegúrate de tener esta clase en tu CSS
+        }
+        
+        // Después de la transición, quitar el splash screen y redirigir a /login
+        setTimeout(() => {
+          this.showSplashScreen = false;
+          localStorage.setItem('hasSeenSplashScreen', 'true'); // Marcar que se ha visto el splash screen
+          
+          // Redirigir a /login
+          this.router.navigate(['/login']);
+        }, 1000); // Esperar a que la transición termine (1s)
+      }, 3000); // Mostrar el splash screen por 3 segundos
+    } else {
+      // Si ya se ha visto el splash screen, redirigir inmediatamente a /login
+      this.showSplashScreen = false;
+      this.router.navigate(['/login']);
+    }
   }
 }
