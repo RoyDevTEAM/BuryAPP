531984c7700325f62980a4e1cf2b9b0c50175a9e
RoyDevTEAM <roidercito12345@gmail.com>
2024-09-07 16:16:59 -0400

    animacion mejorada

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   showSplashScreen = true; // Inicialmente mostrar el splash screen
 
-  constructor() {
+  constructor() {}
+
+  ngOnInit() {
     this.initializeApp();
   }
 
   initializeApp() {
-    setTimeout(() => {
-      // Aplicar clase de transición para ocultar el splash screen
-      const splashElement = document.querySelector('app-splash-screen');
-      if (splashElement) {
-        splashElement.classList.add('hidden');
-      }
-  
-      // Después de la transición, quitar el splash screen
+    // Verificar si ya se ha mostrado el splash screen
+    const hasSeenSplashScreen = localStorage.getItem('hasSeenSplashScreen');
+    
+    if (!hasSeenSplashScreen) {
+      // Mostrar el splash screen si es la primera vez
       setTimeout(() => {
-        this.showSplashScreen = false;
-      }, 500); // Esperar a que la transición termine (0.5s)
-    }, 3000); // Mostrar el splash screen por 3 segundos
+        // Aplicar clase de transición para ocultar el splash screen
+        const splashElement = document.querySelector('app-splash-screen');
+        if (splashElement) {
+          splashElement.classList.add('hidden');
+        }
+        
+        // Después de la transición, quitar el splash screen
+        setTimeout(() => {
+          this.showSplashScreen = false;
+          // Marcar que se ha visto el splash screen
+          localStorage.setItem('hasSeenSplashScreen', 'true');
+        }, 500); // Esperar a que la transición termine (0.5s)
+      }, 3000); // Mostrar el splash screen por 3 segundos
+    } else {
+      // Si ya se ha visto el splash screen, no mostrarlo
+      this.showSplashScreen = false;
+    }
   }
 }
