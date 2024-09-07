54684e83781f4dd4f3649c2c9adc73f49c373ec9
RoyDevTEAM <roidercito12345@gmail.com>
2024-09-07 17:15:12 -0400

    animacion falta mejorar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,42 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   showSplashScreen = true; // Inicialmente mostrar el splash screen
 
-  constructor() {}
-
-  ngOnInit() {
+  constructor() {
     this.initializeApp();
   }
 
   initializeApp() {
-    // Verificar si ya se ha mostrado el splash screen
-    const hasSeenSplashScreen = localStorage.getItem('hasSeenSplashScreen');
-    
-    if (!hasSeenSplashScreen) {
-      // Mostrar el splash screen si es la primera vez
+    setTimeout(() => {
+      // Aplicar clase de transición para ocultar el splash screen
+      const splashElement = document.querySelector('app-splash-screen');
+      if (splashElement) {
+        splashElement.classList.add('hidden');
+      }
+  
+      // Después de la transición, quitar el splash screen
       setTimeout(() => {
-        // Aplicar clase de transición para ocultar el splash screen
-        const splashElement = document.querySelector('app-splash-screen');
-        if (splashElement) {
-          splashElement.classList.add('hidden');
-        }
-        
-        // Después de la transición, quitar el splash screen
-        setTimeout(() => {
-          this.showSplashScreen = false;
-          // Marcar que se ha visto el splash screen
-          localStorage.setItem('hasSeenSplashScreen', 'true');
-        }, 500); // Esperar a que la transición termine (0.5s)
-      }, 3000); // Mostrar el splash screen por 3 segundos
-    } else {
-      // Si ya se ha visto el splash screen, no mostrarlo
-      this.showSplashScreen = false;
-    }
+        this.showSplashScreen = false;
+      }, 500); // Esperar a que la transición termine (0.5s)
+    }, 5000); // Mostrar el splash screen por 3 segundos
   }
 }
