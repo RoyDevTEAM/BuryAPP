64d0eac3f578406d5aad4d9d085eae6d11b22f57
RoyDevTEAM <roidercito12345@gmail.com>
2024-09-11 17:26:32 -0400

    error producccion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,8 @@ import { RouteReuseStrategy } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { HttpClientModule } from '@angular/common/http';  // Importa HttpClientModule
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AngularFireModule } from '@angular/fire/compat';
-import { AngularFireAuthModule } from '@angular/fire/compat/auth';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
@@ -19,8 +16,7 @@ import { environment } from '../environments/environment';
     IonicModule.forRoot(),
     AppRoutingModule,
     HttpClientModule,  // Agrega HttpClientModule aquí
-    AngularFireModule.initializeApp(environment.firebaseConfig), // Inicializa Firebase
-    AngularFireAuthModule // Importa el módulo de autenticación
+   
   ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy   }],
   bootstrap: [AppComponent],
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,4 @@
 // src/environments/environment.ts
 export const environment = {
   production: false,
-  firebaseConfig: {
-    apiKey: "AIzaSyDq9D9CHdQPKPbgKss2ciP8LTtxw1ZBBwo",
-  authDomain: "buriapp.firebaseapp.com",
-  projectId: "buriapp",
-  storageBucket: "buriapp.appspot.com",
-  messagingSenderId: "793482122007",
-  appId: "1:793482122007:web:a011440bb990da9f0c1d52"
-  }
 };
