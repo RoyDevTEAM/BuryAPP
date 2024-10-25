29ea4b3cb04a30369d444a01cdae06aeb768fdbd
Elias Campos Justiniano <eliascj.github@gmail.com>
2024-10-25 14:30:55 -0400

    modificando datelles tecnicos

--- src/app/components/formreserva/formreserva.page.ts
+++ src/app/components/formreserva/formreserva.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { BaresService } from 'src/app/services/bares.service';
+import { AuthService } from 'src/app/services/auth.service'; // Servicio de autenticación
 
 @Component({
   selector: 'app-formreserva',
@@ -8,35 +9,47 @@ import { BaresService } from 'src/app/services/bares.service';
   styleUrls: ['./formreserva.page.scss'],
 })
 export class FormreservaPage implements OnInit {
-  nombre: string = '';
+  nombreUsuario: string = ''; // Nuevo campo para el nombre del usuario obtenido del token
   numeroPersonas: number | null = null;
   fecha: string = ''; 
   hora: string | null = null;
   idMesa: string | null = null;
-  idBar: number | null = null; // Nuevo campo para el ID del bar
-  nombreBar: string = ''; // Nuevo campo para almacenar el nombre del bar
+  idBar: number | null = null; // ID del bar
+  nombreBar: string = ''; // Nombre del bar
   toastMessage: string = '';
 
   constructor(
     private route: ActivatedRoute,
     private router: Router,
-    private baresService: BaresService // Inyecta el servicio de bares
+    private baresService: BaresService,
+    private authService: AuthService // Inyecta el servicio de autenticación
   ) {}
 
   ngOnInit() {
+    // Obtener ID de mesa y bar desde los parámetros de la ruta
     this.route.paramMap.subscribe(params => {
       this.idMesa = params.get('id');
-      this.idBar = +params.get('barId')!; // Asume que el ID del bar se pasa como parámetro
+      this.idBar = +params.get('barId')!;
 
-      // Obtiene el nombre del bar por su ID
+      // Obtener el nombre del bar
       this.obtenerNombreBar(this.idBar);
     });
+
+    // Obtener el nombre del usuario desde el token
+    this.authService.getUserInfo().subscribe(
+      (usuario) => {
+        this.nombreUsuario = usuario.name; // Asigna el nombre del usuario
+      },
+      (error) => {
+        console.error('Error al obtener la información del usuario:', error);
+      }
+    );
   }
 
   obtenerNombreBar(id: number) {
     this.baresService.getBar(id).subscribe(
       (bar) => {
-        this.nombreBar = bar.Nombre; // Asigna el nombre del bar
+        this.nombreBar = bar.Nombre;
       },
       (error) => {
         console.error('Error al obtener el nombre del bar:', error);
@@ -45,8 +58,8 @@ export class FormreservaPage implements OnInit {
   }
 
   enviarReserva() {
-    if (this.nombre && this.numeroPersonas && this.fecha && this.hora && this.idMesa) {
-      const mensaje = `Reserva: ${this.nombreBar}\nNombre: ${this.nombre}\nCantidad de Personas: ${this.numeroPersonas}\nFecha: ${this.fecha}\nHora: ${this.hora}\n Nro de Mesa: ${this.idMesa}`;
+    if (this.nombreUsuario && this.numeroPersonas && this.fecha && this.hora && this.idMesa) {
+      const mensaje = `Reserva: ${this.nombreBar}\nNombre: ${this.nombreUsuario}\nCantidad de Personas: ${this.numeroPersonas}\nFecha: ${this.fecha}\nHora: ${this.hora}\n Nro de Mesa: ${this.idMesa}`;
       const numeroTelefono = '+59163551738';
       this.enviarMensajeAWhatsApp(numeroTelefono, mensaje);
     } else {
